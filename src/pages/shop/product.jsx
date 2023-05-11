67bb567876ec4164d08c303bd01ad88f32f9e710
Umeano Buchi <ibumeano@gmail.com>
2023-05-11 20:28:02 +0100

    done with basic responsive styling

--- src/pages/shop/product.jsx
+++ src/pages/shop/product.jsx
@@ -1,30 +1,35 @@
 import React from "react";
 import { Plus } from "phosphor-react";
+import "./product.css";
 
 export const Product = (props) => {
   const { id, title, price, thumbnail, discountPercentage, stock } = props.data;
   return (
     <div className="product">
-      <img src={thumbnail} alt="" />
+      <div
+        id="imgcontainer"
+        style={{ backgroundImage: `url(${thumbnail})` }}
+      ></div>
       <h3>{title}</h3>
       <div className="prices">
         <p>${price}</p>
         <small>{discountPercentage}% off</small>
       </div>
       <p>{stock} units left.</p>
-      <button>
-        <div className="addtocart">
-          <Plus />
-          Add to Cart
-        </div>
-      </button>
-      <button>
-        <div className="buynow">
-          <Plus />
-          Order Now
-        </div>
-      </button>
-      <br />
+      <div className="buttons">
+        <button>
+          <div className="addtocart">
+            <Plus />
+            Add to Cart
+          </div>
+        </button>
+        <button>
+          <div className="buynow">
+            <Plus />
+            Order Now
+          </div>
+        </button>
+      </div>
     </div>
   );
 };
--- src/products.js
+++ src/products.js
@@ -1,7 +1,7 @@
 export const getProducts = async () => {
   try {
     const response = await fetch(
-      "https://dummyjson.com/products?limit=25&select=title,price,thumbnail,discountPercentage,stock"
+      "https://dummyjson.com/products?limit=28&select=title,price,thumbnail,discountPercentage,stock"
     );
     const json = await response.json();
     return json.products;
