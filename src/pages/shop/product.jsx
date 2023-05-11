0bff2029a02b5c53a39eca660fbc71f1547b2512
Umeano Buchi <ibumeano@gmail.com>
2023-05-11 19:17:52 +0100

    Displaying product skeleton

--- src/pages/shop/product.jsx
+++ src/pages/shop/product.jsx
@@ -1,8 +1,30 @@
 import React from "react";
+import { Plus } from "phosphor-react";
 
 export const Product = (props) => {
-  const { id, title, price, thumbnail } = props.data;
-  return <div className="product">
-    
-  </div>;
+  const { id, title, price, thumbnail, discountPercentage, stock } = props.data;
+  return (
+    <div className="product">
+      <img src={thumbnail} alt="" />
+      <h3>{title}</h3>
+      <div className="prices">
+        <p>${price}</p>
+        <small>{discountPercentage}% off</small>
+      </div>
+      <p>{stock} units left.</p>
+      <button>
+        <div className="addtocart">
+          <Plus />
+          Add to Cart
+        </div>
+      </button>
+      <button>
+        <div className="buynow">
+          <Plus />
+          Order Now
+        </div>
+      </button>
+      <br />
+    </div>
+  );
 };
--- src/products.js
+++ src/products.js
@@ -1,7 +1,7 @@
 export const getProducts = async () => {
   try {
     const response = await fetch(
-      "https://dummyjson.com/products?limit=25&select=title,price,thumbnail"
+      "https://dummyjson.com/products?limit=25&select=title,price,thumbnail,discountPercentage,stock"
     );
     const json = await response.json();
     return json.products;
