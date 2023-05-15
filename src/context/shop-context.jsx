7ec99b41a05faaffa0212bb1964db5d461c73b91
Umeano Buchi <ibumeano@gmail.com>
2023-05-15 17:30:23 +0100

    Added shop context

---
+++ src/context/shop-context.jsx
@@ -0,0 +1,30 @@
+import React, { createContext, useState } from "react";
+import { getProducts } from "../products";
+const PRODUCTS = await getProducts();
+
+export const ShopContext = createContext(null);
+const getDefaultCart = () => {
+  let cart = {};
+  for (let i = 1; i < PRODUCTS.length + 1; i++) {
+    cart[i] = 0;
+  }
+  return cart;
+};
+
+const [cartItems, setCartItems] = useState(getDefaultCart());
+
+export const ShopContextProvider = (props) => {
+  const addToCart = (itemId) => {
+    setCartItems((prev) => ({ ...prev, [itemId]: prev[itemId] + 1 }));
+  };
+  const removeFromCart = (itemId) => {
+    setCartItems((prev) => ({ ...prev, [itemId]: prev[itemId] - 1 }));
+  };
+
+  const contextValue = { cartItems, addToCart, removeFromCart };
+  return (
+    <ShopContext.Provider value={contextValue}>
+      {props.children}
+    </ShopContext.Provider>
+  );
+};
